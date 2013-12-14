e3c94b66d3ec3565ee3bd67472c5b7e10dfedb4f
Mikhail Itkin <itkin.m@gmail.com>
2013-12-14 05:27:41 +0100

    [install_vimrc] added dependencies

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -4,3 +4,10 @@ ln -s -f `pwd`/gvimrc $HOME/.gvimrc
 cd ~/.vim
 git submodule init
 git submodule update
+
+    # sudo pip install flake8
+    # sudo pip install git+git://github.com/nvie/nose-machineout.git#egg=nose_machineout
+    # sudo pip install jedi
+    # sudo pip install vim_bridge
+    # sudo apt-get install exuberant-ctags
+
