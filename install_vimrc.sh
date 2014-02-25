fd9c8d1d0592bb8932d36a72227a5dada5a1c622
mitkin <mitkin@mitkin-vb.(none)>
2014-02-25 21:47:50 +0100

    [install] uncommented commands with the sudo prefix

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -5,9 +5,9 @@ cd ~/.vim
 git submodule init
 git submodule update
 
-    # sudo pip install flake8
-    # sudo pip install git+git://github.com/nvie/nose-machineout.git#egg=nose_machineout
-    # sudo pip install jedi
-    # sudo pip install vim_bridge
-    # sudo apt-get install exuberant-ctags
+sudo pip install flake8
+sudo pip install git+git://github.com/nvie/nose-machineout.git#egg=nose_machineout
+sudo pip install jedi
+sudo pip install vim_bridge
+sudo apt-get install exuberant-ctags
 
