b29204bc342a0bcf64bc1cc9858e0b5799a0888b
Mikhail Itkin <itkin.m@gmail.com>
2014-11-07 13:26:42 +0000

    updated dotvim installation instuctions script

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -2,11 +2,11 @@ ln -s -f `pwd`/vimrc $HOME/.vimrc
 ln -s -f `pwd`/gvimrc $HOME/.gvimrc
 
 cd ~/.vim
-git submodule init
-git submodule update
 
 sudo pip install flake8
 sudo pip install git+git://github.com/nvie/nose-machineout.git#egg=nose_machineout
 sudo pip install jedi
 sudo pip install vim_bridge
 sudo apt-get install exuberant-ctags
+
+git submodule update --recursive --init
